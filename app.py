d7c89787c35bff6fddaa25d44ffeda5facda98c5
wan-ek <pluem.pluem.pluem111@gmail.com>
2024-08-12 19:20:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -154,5 +154,3 @@ if uploaded_file is not None:
         st.write(f"ความมั่นใจของการทำนาย: {confidence:.2f}")
     except Exception as e:
         st.error(f"Error processing uploaded file: {e}")
-
-
